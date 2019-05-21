3b1feffade1ced2c189dbebbb6a09039a024de59
Hugh Rawlinson <hughr2005@gmail.com>
2019-05-21 09:27:28 +0200

    Characterize library api

--- test/wekinator-node.test.js
+++ test/wekinator-node.test.js
@@ -3,3 +3,30 @@ const wn = require('../index.js');
 test('exports something', function() {
   expect(wn).not.toBeNull();
 });
+
+test('exports the right functions', function() {
+  expect(Object.keys(wn.prototype).sort()).toEqual([
+    "cancelTrain",
+    "close",
+    "connect",
+    "deleteAllExamples",
+    "disableModelRecording",
+    "disableModelRunning",
+    "disconnect",
+    "enableModelRecording",
+    "enableModelRunning",
+    "inputs",
+    "outputs",
+    "selectInputsForOutput",
+    "setInputNames",
+    "setOutputNames",
+    "startDtwRecording",
+    "startRecording",
+    "startRunning",
+    "stopDtwRecording",
+    "stopRecording",
+    "stopRunning",
+    "train",
+    "trainOnData"
+  ]);
+});
